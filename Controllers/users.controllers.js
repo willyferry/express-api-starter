82d363008b35cb4839d62d6ae56d98748425a549
Willy Ferry <willyferry001@gmail.com>
2022-05-17 12:05:54 +0700

    done CRUD Users

--- Controllers/users.controllers.js
+++ Controllers/users.controllers.js
@@ -1,24 +1,53 @@
+const User = require("../Models/users");
+
 const reads = async (req, res) => {
+    const users = await User.find();
+    
     res.json({
-        message: 'Get All Users Data'
+        message: 'Get All Users Data',
+        data: users
     });
 }
 
 const create = async (req, res) => {
-    res.json({
-        message: 'Create New User'
-    })
+    const user = await new User({
+        name: req.body.name,
+        email: req.body.email,
+    });
+
+    await user.save(function(err, data) {
+        if (err) {
+            res.json({
+                message: 'Failed to create user: ' + err,
+                error: err
+            });
+        } else {
+            res.json({
+                message: 'Success create user',
+                data: data
+            });
+        }
+    });
 }
 
 const update = async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        email: req.body.email,
+    });
+
     res.json({
-        message: 'Update User'
-    })
+        message: 'Success update user',
+        data: user
+    });
 }
 
 const destroy = async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
     res.json({
-        message: 'Delete User'
+        message: 'Success delete user',
+        data: user  
     });
 }
 
--- Models/users.js
+++ Models/users.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name: String,
     email: String,
+    __v: false
+}, {
+    timestamps: true,
+    versionKey: false
 });
 
 module.exports = mongoose.model("User", userSchema);
