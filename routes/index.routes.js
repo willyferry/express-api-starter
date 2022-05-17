cd0675bf46155b8b3197376377d1f5ed684aff3f
Willy Ferry <willyferry001@gmail.com>
2022-05-17 11:44:50 +0700

    Delete routes directory

--- routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const indexControllers = require('../Controllers/index.controllers');
-const router = express.Router();
-
-router.get('/', indexControllers.index);
-
-module.exports = router;
